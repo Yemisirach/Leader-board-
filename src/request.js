4c49606dd349aa24ee832cea1ec0d3ee18e69b4e
Yemisirach <yemsrach3723@gmail.com>
2023-02-16 09:29:38 +0300

    Add API score url

---
+++ src/request.js
@@ -0,0 +1,4 @@
+const base_url =
+  "https://us-central1-js-capstone-backend.cloudfunctions.net/api/";
+const API_SCORES_URL = base_url.concat("games/zCRkYjTWW67HHx57jSh1/scores/");
+export default API_SCORES_URL;
